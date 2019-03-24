0fb7e562abe93e8b7be0a6aede558a9fabcaf151
Akihiko Kusanagi <nagi@nagi-p.com>
2019-03-24 19:35:16 +0800

    Add check for point positions

--- src/elements/element.stylePoint.js
+++ src/elements/element.stylePoint.js
@@ -3,11 +3,21 @@
 import Chart from 'chart.js';
 import styleHelpers from '../helpers/helpers.style';
 
+var helpers = Chart.helpers;
+
+// For Chart.js 2.7.3 backward compatibility
+var isPointInArea = helpers.canvas._isPointInArea || function(point, area) {
+	var epsilon = 1e-6;
+
+	return point.x > area.left - epsilon && point.x < area.right + epsilon &&
+		point.y > area.top - epsilon && point.y < area.bottom + epsilon;
+};
+
 var Point = Chart.elements.Point;
 
 export default Point.extend({
 
-	draw: function() {
+	draw: function(chartArea) {
 		var me = this;
 		var args = arguments;
 		var chart = me._chart;
@@ -17,6 +27,10 @@ export default Point.extend({
 		var drawCallback = function() {
 			Point.prototype.draw.apply(me, args);
 		};
+
+		if (vm.skip || chartArea !== undefined && !isPointInArea(vm, chartArea)) {
+			return;
+		}
 
 		styleHelpers.drawShadow(chart, vm.shadowOffsetX, vm.shadowOffsetY,
 			vm.shadowBlur, vm.shadowColor, drawCallback, true);
