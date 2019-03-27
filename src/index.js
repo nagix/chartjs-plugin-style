612cd360a8dad5dd2c97bd53a5a57ab297db5945
Akihiko Kusanagi <nagi@nagi-p.com>
2019-03-27 12:48:10 +0800

    Remove unnecessary global properties

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 'use strict';
 
 import Chart from 'chart.js';
-
-import StyleTooltip from './core/core.styleTooltip';
-
-import StyleHelper from './helpers/helpers.style';
-
-import StyleArcElement from './elements/element.styleArc';
-import StyleLineElement from './elements/element.styleLine';
-import StylePointElement from './elements/element.stylePoint';
-import StyleRectangleElement from './elements/element.styleRectangle';
-
 import StylePlugin from './plugins/plugin.style';
-
-Chart.StyleTooltip = StyleTooltip;
-
-Chart.helpers.style = StyleHelper;
-
-Chart.elements.StyleArc = StyleArcElement;
-Chart.elements.StyleLine = StyleLineElement;
-Chart.elements.StylePoint = StylePointElement;
-Chart.elements.StyleRectangle = StyleRectangleElement;
 
 Chart.plugins.register(StylePlugin);
 
